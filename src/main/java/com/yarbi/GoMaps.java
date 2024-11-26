3a76eb1e940b8068b7466f95ea5e883680322e77
faiz-oussama <faizouss123@gmail.com>
2024-11-26 21:27:36 +0100

    done!!

--- src/main/java/com/yarbi/GoMaps.java
+++ src/main/java/com/yarbi/GoMaps.java
@@ -11,35 +11,29 @@ import com.teamdev.jxbrowser.view.javafx.BrowserView;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class GoMaps extends Application {
     @Override
     public void start(Stage stage) {
-        // Set up JxBrowser Engine with hardware acceleration
         EngineOptions options = EngineOptions.newBuilder(HARDWARE_ACCELERATED)
                 .licenseKey("OK6AEKNYF2JOKOYOR3NKEBSBKT2JTJI0XI3O7Z26JLXVGTSF5N9GQKVZ0VF0LWSMEEK4JWCN9N5KYRDKD23XMW8B6Q0P1IGPE0HOE4DQVEKPEQ9CV2RH8BHGK9U8V4Q1470XLIP711VIGED5Y")
                 .build();
         Engine engine = Engine.newInstance(options);
 
-        // Create the browser instance
         Browser browser = engine.newBrowser();
-
-        // Create the BrowserView to embed in JavaFX scene
         BrowserView view = BrowserView.newInstance(browser);
 
-        // Set up the layout and scene
         Scene scene = new Scene(new BorderPane(view), 1280, 700);
 
-        // Start Jetty server to serve the HTML file
-        Server server = new Server(8080);  // Jetty server on port 8080
+        Server server = new Server(8080);
         ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        handler.setResourceBase("src/main/resources");  // Serve from resources folder
-        handler.setContextPath("/");  // Context path at root
+        handler.setResourceBase("src/main/resources");
+        handler.setContextPath("/");
         server.setHandler(handler);
 
-        // Start Jetty server
         try {
             server.start();
             System.out.println("Server started on http://localhost:8080");
@@ -47,20 +41,19 @@ public class GoMaps extends Application {
             e.printStackTrace();
         }
 
-        // Load the HTML file via the Jetty server URL
-        String url = "http://127.0.0.1:5500/demo/src/main/resources/javascript/index.html"; // Jetty serves this file
+        String url = "http://127.0.0.1:5500/demo/src/main/resources/javascript/index.html";
         browser.navigation().loadUrl(url);
 
-        // Set up the JavaFX window
+        stage.getIcons().add(new Image(getClass().getResource("/javascript/l.png").toExternalForm()));
+
         stage.setScene(scene);
-        stage.setTitle("Log Monitoring Dashboard");
+        stage.setTitle("GoMaps");
         stage.show();
-        // Ensure the server shuts down when the application closes
         stage.setOnCloseRequest(event -> {
             browser.close();
             engine.close();
             try {
-                server.stop();  // Stop the Jetty server on exit
+                server.stop();
             } catch (Exception e) {
                 e.printStackTrace();
             }
